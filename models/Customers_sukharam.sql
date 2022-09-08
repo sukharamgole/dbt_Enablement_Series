2e96da3920673133a0dcbeacc3b5dad727000431
sukharam.gole@infosys.com <sukharam.gole@infosys.com>
2022-09-08 04:59:00 +0000

    Update the Query and change the default mode of data view from view to Table

---
+++ models/Customers_sukharam.sql
@@ -0,0 +1,6 @@
+{{
+    config(
+        materialized ='table'
+    )
+}}
+SELECT * FROM `poc-dna-bigbets-foundry-47416.dna_foundry_staging_dataset.Customers_sukharam` 
---
+++ models/Payments_sukharam.sql
@@ -0,0 +1,7 @@
+{{
+    config(
+        materialized ='table'
+    )
+}}
+
+SELECT * FROM `poc-dna-bigbets-foundry-47416.dna_foundry_staging_dataset.Payments_sukharam`
